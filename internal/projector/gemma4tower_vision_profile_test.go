b87a4034b79321dc08ee667d659afde24a921e8c
jeffmeloy <128848756+jeffmeloy@users.noreply.github.com>
2026-08-14 03:32:29 -0400

    projector: bind E4B tower input affine

--- internal/projector/gemma4tower_vision_profile_test.go
+++ internal/projector/gemma4tower_vision_profile_test.go
@@ -2,14 +2,14 @@ package projector
 
 import "testing"
 
-func TestGemma4TowerVisionNormalizationUsesProfile(t *testing.T) {
+func TestGemma4TowerVisionInputAffineUsesProfile(t *testing.T) {
 	pixels := []float32{0.5, 0.25, 0.75, 1, 0, 0.5}
-	got := gemma4TowerNormalizePixels(
+	got := gemma4TowerInputAffine(
 		pixels,
-		[3]float32{0.25, 0.5, 0.75},
-		[3]float32{0.25, 0.25, 0.5},
+		[3]float32{2, 4, 0.5},
+		[3]float32{-1, -1, 0.25},
 	)
-	want := []float32{1, -1, 0, 3, -2, -0.5}
+	want := []float32{0, 0, 0.625, 1, -1, 0.5}
 	for index := range want {
 		if got[index] != want[index] {
 			t.Fatalf("normalized[%d]=%g want=%g", index, got[index], want[index])
